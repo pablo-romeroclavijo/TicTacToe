e27ace883a7e12608d6482d48b44fd2a90cb9b13
pablo-romeroclavijo <136720541+pablo-romeroclavijo@users.noreply.github.com>
2023-09-15 11:17:34 +0100

    Debuged medium level and optimized console.text

--- tictactoe.js
+++ tictactoe.js
@@ -12,7 +12,7 @@ let remainingMoves = [1,2,3,4,5,6,7,8,9]
 
 function difficulty (user){
     difficulties = ['easy', 'medium']
-    level = prompt(`Choose game difficulty for COMPUTER${user.ID} (easy/medium/hard): `)
+    level = prompt(`Choose game difficulty for Computer ${user.ID} (easy/medium/hard): `)
     while (!(difficulties.includes(level))){
         level = prompt(`Level not coded\nChoose game difficulty for COMPUTER${user.ID} (easy/medium/hard): `)
     }
@@ -28,37 +28,46 @@ function computerMove(user){
         const randomIndex = Math.floor(Math.random()*remainingMoves.length);
         return remainingMoves[randomIndex]
 
-        case(medium):
+        case("medium"):
             if(remainingMoves.length == 9){                 //computer plays first
                 const corners = [1, 3, 7, 9]
                 const randomCorner = corners[Math.floor(Math.random()*3)]
                 return randomCorner
             }else if(remainingMoves.length == 8) {          //computer plays second
-                const randomChoice = Math.floor(Math.random()*remainingMoves.length);
+                const randomIndex = Math.floor(Math.random()*remainingMoves.length);
                 return remainingMoves[randomIndex]
             }else{
-                
                 let selfWinningMoves = []                   // find selfWinning movemetns
-                for(movement in remainingMoves){
+                for(index in remainingMoves){
+                    let movement = remainingMoves[index]
+                   // console.log(movement, remainingMoves)
                     let tempGrid = {...gameGrid};
                     tempGrid[movement] = user.ID
-                    if(verify(user, tempGrid).length !== 0){
+                   // console.log('self TempGrid', tempGrid, gameGrid)
+                    if(verify(user.ID, tempGrid).length !== 0){
+                        //console.log(verify(user.ID, tempGrid))
                         selfWinningMoves.push(movement)
                     }
-                
                 }
+                //console.log("selfWinningMoves", selfWinningMoves)
                 if(selfWinningMoves.length !==0){           //returns any of the movements that grant victotry
                     const randomIndex = Math.floor(Math.random()*selfWinningMoves.length);
                     return selfWinningMoves[randomIndex]}
                 else {
                     let oponentWinningMoves = [];
-                    if (user.ID == 1){let ID = 2}else{let ID = 1} 
-                    for(movement in remainingMoves){
+                    let ID = undefined
+                    if (user.ID == 1){ID = 2}else{ID = 1} 
+                    for(index in remainingMoves){
+                        let movement = remainingMoves[index]
+                       // console.log(movement, remainingMoves)
                         let tempGrid = {...gameGrid};
                         tempGrid[movement] = ID
-                        if(verify(user, tempGrid).length !== 0){
+                       // console.log('opo TempGrid', tempGrid, gameGrid)
+                        if(verify(ID, tempGrid).length !== 0){
+                           // console.log(verify(ID, tempGrid))
                             oponentWinningMoves.push(movement)}
                     }
+                    //console.log('oponentWinningMoves', oponentWinningMoves)
         
                     if(oponentWinningMoves.length !==0){        //returns any of the movements that grant victory to the oponent
                         const randomIndex = Math.floor(Math.random()*oponentWinningMoves.length);
@@ -78,11 +87,11 @@ function move(user){
     const type = user.type
     let choice = null
     switch(type){
-        case('human'):
-            choice = Number(prompt(`User${user.ID} Choose your move: `))
+        case('Human'):
+            choice = Number(prompt(` ${user.symbol}${user.symbol} ${user.type} ${user.ID} ${user.symbol}${user.symbol}  Choose your move: `))
             break;
 
-        case('computer'):
+        case('Computer'):
             choice = computerMove(user);
             break;
     }
@@ -96,7 +105,7 @@ function move(user){
     
 }
 
-function verify(user, grid){
+function verify(ID, grid){
     let g = (i) => grid[i]
     let combinations = {
         row1 : [[1,2,3], [g(1),g(2),g(3)]],
@@ -112,18 +121,17 @@ function verify(user, grid){
         dig2 : [[3,5,7],[g(3),g(5),g(7)]]}
        
 
-    let winComb = [user.ID, user.ID, user.ID]
+    let winComb = [ID, ID, ID]
     let combvalues = Object.values(combinations).map(elem => elem[1])
 
 
     let results = []
     for(comb in combinations){
         if (combinations[comb][1].toString() == winComb.toString()){
-            let result = {ID: user.ID, posiitons:combinations[comb][0], comb: combinations[comb][1]}
+            let result = {ID: ID, posiitons:combinations[comb][0], comb: combinations[comb][1]}
             results.push(result)   
         }
     }
-    console.log(results)
     return results   
 }
 
@@ -131,8 +139,8 @@ function verify(user, grid){
 function graphics(){
     const symbols = {
         0: ' ',
-        1: 'O',
-        2: 'X'}
+        1: user1.symbol,
+        2: user2.symbol}
     
         s = symbols
         let g = gameGrid
@@ -148,28 +156,26 @@ function graphics(){
 
 function game(){
     gameGrid = {...masterGrid}
-    console.log(gameGrid)
     remainingMoves = [...restMoves]
-    console.log(remainingMoves)
-    let user =  (number, typ, dif = null) => {return {ID: number, type: typ, level: dif}}
+    let user =  (number, typ, symbol, dif = null) => {return {ID: number, type: typ, symbol: symbol, level: dif}}
 
   
     const nPlayers = prompt('Number of players (0, 1 or 2):' )
     switch(nPlayers){
         case("2"):
-            user1 = user(1, 'human')
-            user2 = user(2, 'human');
+            user1 = user(1, 'Human', 'X')
+            user2 = user(2, 'Human', 'O');
             break;
 
         case("1"):
-            user1 = user(1, 'human')
-            user2 = user(2, 'computer')
+            user1 = user(1, 'Human', 'X')
+            user2 = user(2, 'Computer', 'O')
             user2.level = difficulty(user2);
             break;
 
         case('0'):
-            user1 = user(1, 'computer')
-            user2 = user(2, 'computer')
+            user1 = user(1, 'computer', 'X')
+            user2 = user(2, 'computer', 'O')
             user1.level = difficulty(user1)
             user2.level = difficulty(user2);
             break;
@@ -185,32 +191,37 @@ function game(){
     const firstPlayer = () =>{
         move(user1);
         graphics();
-        let result = verify(user1, gameGrid)
+        let result = verify(user1.ID, gameGrid)
         if(result.length !==0){
-            console.log('USER 1 WON!!\n\n\n Starting new game');
+            console.log(`${user1.symbol}${user1.symbol} ${user1.type} 1 WON!! ${user1.symbol}${user1.symbol}\n\n\n Starting new game`);
             game()  
         }}
 
     const secondPlayer = () => {
         move(user2); 
         graphics();
-        let result = verify(user2, gameGrid)
+        let result = verify(user2.ID, gameGrid)
         if(result.length !==0){
-            console.log('USER 2 WON!!\n\n\n Starting new game')
+            console.log(`${user2.symbol}${user2.symbol} ${user2.type} 2 WON!! ${user2.symbol}${user2.symbol}\n\n\n Starting new game`)
             game()  
         }}
-
-    console.log(`USER ${randomStart} plays first`)
+    if(randomStart == 1){console.log(`${user1.type} ${randomStart} plays first `)}
+    else{console.log(`${user2.type} ${randomStart} plays first `)}
+    
+    
     while(remainingMoves.length > 0){ 
         if(randomStart == 1){
             firstPlayer()
-            secondPlayer()}
+            if(remainingMoves.length == 0){console.log("It's a TIE\n\nStarting new game"); game()}
+            else{secondPlayer()}
+            }
         else{
             secondPlayer()
-            firstPlayer()
+            if(remainingMoves.length == 0){console.log("It's a TIE\n\nStarting new game"); game()}
+            else{firstPlayer()}
         }
     }
-    console.log("It's a TIE\nStarting new game"); 
+    console.log("It's a TIE\n\nStarting new game"); 
     game()
     }
 
